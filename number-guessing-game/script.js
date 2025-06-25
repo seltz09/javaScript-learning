4b19e58ecc56ef6c8431e96dfdd6d4c30d825e4c
Lucas Faria <farialuc22@gmail.com>
2025-06-25 12:00:00 -0400

    Add files via upload

--- number-guessing-game/script.js
+++ number-guessing-game/script.js
@@ -1,1 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+
+// This is for the computer generating the number 0-9
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+
+// function that will be used to compare the guesses
+function compareGuesses(humanGuess, compGuess, target) {
+  const humanGuessDistance = Math.abs(target - humanGuess);
+  const compGuessDistance = Math.abs(target - compGuess);
+  if (humanGuessDistance < compGuessDistance) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// updating score 
+function updateScore(value) {
+  if (value === "human") {
+    humanScore += 1;
+  } else if (value === "computer") {
+    computerScore += 1;
+  } else {
+    return "there has been an error";
+  }
+}
+
+// advancing the round
+function advanceRound(roundValue) {
+  return (currentRoundNumber += 1);
+}
+
 
